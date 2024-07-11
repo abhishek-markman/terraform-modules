eb69f8a000269f00c88a4a2c8a3629b75eef5be0
abhishek-markman <abhishek@markmanassociates.com>
2024-07-11 15:26:25 +0530

    updated format

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ module "vnet" {
 
   resource_group_name = azurerm_resource_group.main_rg.name
   use_for_each        = var.use_for_each
-  vnet_name           = "${var.name_prefix}-vnet"
+  vnet_name           = "${local.name_prefix}-vnet"
   vnet_location       = var.location
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   tags = {
-    environment = "dev"
+    environment  = "dev"
     DeployedFrom = "terraform"
   }
 }
